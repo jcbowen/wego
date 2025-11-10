48b7cb67c175dc92a3835bc5fe350d750f81389e
Bowen <bowen@jiuchet.com>
2025-11-10 14:22:08 +0800

    更新(开放平台和公众号) 优化用户信息结构体并移除重复定义

--- official_account/types.go
+++ official_account/types.go
@@ -48,9 +48,8 @@ type OAuthUserInfoRequest struct {
 	Lang        string // 返回国家地区语言版本，zh_CN 简体，zh_TW 繁体，en 英语
 }
 
-// OAuthUserInfoResponse 获取用户信息响应
-type OAuthUserInfoResponse struct {
-	core.APIResponse
+// OAuthUserInfo 用户信息
+type OAuthUserInfo struct {
 	OpenID     string   `json:"openid"`            // 用户的唯一标识
 	Nickname   string   `json:"nickname"`          // 用户昵称
 	Sex        int      `json:"sex"`               // 用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
@@ -60,6 +59,12 @@ type OAuthUserInfoResponse struct {
 	HeadImgURL string   `json:"headimgurl"`        // 用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
 	Privilege  []string `json:"privilege"`         // 用户特权信息，json 数组，如微信沃卡用户为（chinaunicom）
 	UnionID    string   `json:"unionid,omitempty"` // 只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段
+}
+
+// OAuthUserInfoResponse 获取用户信息响应
+type OAuthUserInfoResponse struct {
+	core.APIResponse
+	OAuthUserInfo
 }
 
 // OAuthAuthRequest 检验授权凭证（access_token）是否有效请求参数
