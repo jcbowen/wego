8c4509b39c7a8b840c1b37bce2785be55e76bcc4
Bowen <bowen@jiuchet.com>
2025-11-11 16:22:01 +0800

    更新(开放平台) 修正配置字段的JSON和INI标签命名

--- openplatform/config.go
+++ openplatform/config.go
@@ -6,8 +6,8 @@ import (
 
 // Config 微信开放平台配置结构体
 type Config struct {
-	ComponentAppID     string `json:"component_app_id" ini:"component_app_id"`         // 第三方平台appid
-	ComponentAppSecret string `json:"component_app_secret" ini:"component_app_secret"` // 第三方平台appsecret
+	ComponentAppID     string `json:"component_appid" ini:"component_appid"`         // 第三方平台appid
+	ComponentAppSecret string `json:"component_appsecret" ini:"component_appsecret"` // 第三方平台appsecret
 	ComponentToken     string `json:"component_token" ini:"component_token"`           // 消息校验Token
 	EncodingAESKey     string `json:"encoding_aes_key" ini:"encoding_aes_key"`         // 消息加解密Key
 	RedirectURI        string `json:"redirect_uri" ini:"redirect_uri"`                 // 授权回调URI
